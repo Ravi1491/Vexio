c794cab99c753418f83ba73542891e4d310934b6
Ravi Singh <ravi149185@gmail.com>
2023-12-16 17:21:01 +0530

    Add cors package for enabling Cross-Origin Resource Sharing (CORS)

--- vexio-app-server/src/index.js
+++ vexio-app-server/src/index.js
@@ -3,6 +3,7 @@ import express from "express";
 
 import logger from "./utils/logger";
 import { port } from "../config/default";
+import cors from "cors";
 
 const app = express();
 const PORT = port;
