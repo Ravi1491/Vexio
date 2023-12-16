20145c75021f622c8afd7d78b5ca14929a179616
Shalini <chauhanshaliniuk@gmail.com>
2023-12-16 12:23:32 +0530

    material ui

--- vexio-app-web/src/App.js
+++ vexio-app-web/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
+import Login from "./pages/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
