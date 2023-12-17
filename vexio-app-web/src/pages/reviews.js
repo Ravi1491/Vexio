67fce3211eb56d1e3cd200108f9408777180b47f
Ishita <ishita@gluelabs.com>
2023-12-17 14:49:33 +0530

    changes done

--- vexio-app-web/src/pages/reviews.js
+++ vexio-app-web/src/pages/reviews.js
@@ -1,74 +1,104 @@
+import { useState } from "react";
+import mail from "../assets/mail.png";
+import send from "../assets/send.png";
+import SendRequest from "../components/SendRequestModal";
+
 export default function Reviews() {
+  let [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div
-      style={{
-        padding: "20px",
-      }}
-    >
-      <div
-        style={{
-          fontSize: "22px",
-          fontWeight: 500,
-          color: "#012970",
-        }}
-      >
-        Review Requests
+    <div className="flex flex-col p-5">
+      <div className="flex justify-between items-center">
+        <span className="text-[30px] text-[#012970] font-medium">
+          Reviews Overview
+        </span>
+        <div
+          className="bg-[#FF4495] px-4 py-3 rounded-full cursor-pointer hover:bg-opacity-70"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          <span className="text-white">+ New Request</span>
+        </div>
       </div>
-      <div
-        style={{
-          marginTop: "20px",
-        }}
-      >
-        <table style={{}}>
-          <thead
-            style={{
-              backgroundColor: "#FEDBEE",
-            }}
-          >
-            <tr
-              style={{
-                backgroundColor: "#FEDBEE",
-              }}
-            >
-              <td
-                style={{
-                  padding: "10px 15px",
-                  borderRadius: "6px",
-                }}
-              >
-                Order Id
-              </td>
-              <td
-                style={{
-                  padding: "10px 15px",
-                  borderRadius: "6px",
-                }}
-              >
-                Product
-              </td>
-              <td
-                style={{
-                  padding: "10px 15px",
-                  borderRadius: "6px",
-                }}
-              >
-                Customer
-              </td>
-              <td
-                style={{
-                  padding: "10px 15px",
-                  borderRadius: "6px",
-                }}
-              >
-                Action
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>No Orders yet!</tr>
-          </tbody>
+      <div className="-mt-2">
+        <span className="text-sm text-gray-500">
+          Get reviews overview at one place
+        </span>
+      </div>
+      <div>
+        <table className="rounded mt-10 w-full">
+          <tr className="bg-[#FF4495] bg-opacity-20">
+            <th className="md:px-10 px-5 py-5 text-left text-gray-500 ">
+              Order Id
+            </th>
+            <th className="md:px-10 px-5 py-5 text-left text-gray-500">
+              Product
+            </th>
+            <th className="md:px-10 px-5 py-5 text-left text-gray-500">
+              Customer Email
+            </th>
+            <th className="md:px-10 px-5 py-5 text-left text-gray-500">
+              Action
+            </th>
+          </tr>
+          <tr className="bg-gray-300 bg-opacity-30 text-sm text-gray-500">
+            <td className="md:p-10 sm:p-5 p-1">4523</td>
+            <td className="md:p-10 sm:p-5 p-1 line-clamp-1">
+              ADIDAS | CLASSIC BACKPACK | LEGEND INK MULTICOLOUR
+            </td>
+            <td className="md:p-10 sm:p-5 p-1 text-blue-700">
+              <div className="flex items-center gap-2">
+                <img src={mail} alt="mail" width={20} height={20} />
+                ishitaarora382@gmail.com
+              </div>
+            </td>
+            <td className="md:p-10 sm:p-5 p-1">
+              <div className="flex items-center gap-2">
+                <img src={send} alt="mail" width={20} height={20} />
+                Send
+              </div>
+            </td>
+          </tr>
+          <tr className="bg-gray-300 bg-opacity-30 text-sm text-gray-500">
+            <td className="md:p-10 sm:p-5 p-1">4523</td>
+            <td className="md:p-10 sm:p-5 p-1 line-clamp-1">
+              ADIDAS | CLASSIC BACKPACK | LEGEND INK MULTICOLOUR
+            </td>
+            <td className="md:p-10 sm:p-5 p-1 text-sm text-blue-700">
+              <div className="flex items-center gap-2">
+                <img src={mail} alt="mail" width={20} height={20} />
+                ishitaarora382@gmail.com
+              </div>
+            </td>
+            <td className="md:p-10 sm:p-5 p-1">
+              <div className="flex items-center gap-2">
+                <img src={send} alt="mail" width={20} height={20} />
+                Send
+              </div>
+            </td>
+          </tr>
+          <tr className="bg-gray-300 bg-opacity-30 text-sm text-gray-500">
+            <td className="md:p-10 sm:p-5 p-1">4523</td>
+            <td className="md:p-10 sm:p-5 p-1 line-clamp-1">
+              ADIDAS | CLASSIC BACKPACK | LEGEND INK MULTICOLOUR
+            </td>
+            <td className="md:p-10 sm:p-5 p-1 text-sm text-blue-700">
+              <div className="flex items-center gap-2">
+                <img src={mail} alt="mail" width={20} height={20} />
+                ishitaarora382@gmail.com
+              </div>{" "}
+            </td>
+            <td className="md:p-10 sm:p-5 p-1">
+              <div className="flex items-center gap-2">
+                <img src={send} alt="mail" width={20} height={20} />
+                Send
+              </div>
+            </td>
+          </tr>
         </table>
       </div>
+      <SendRequest isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   );
 }
