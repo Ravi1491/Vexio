ed2a1863a6acc799e4678184799030ae948da94f
Sakshamkhandelwal123 <sakshamkhandelwal2001@gmail.com>
2023-12-17 14:28:53 +0530

    Added Logger

--- vexio-app-server/src/cron/index.js
+++ vexio-app-server/src/cron/index.js
@@ -1,7 +1,8 @@
 import * as cron from "node-cron";
+import logger from "../utils/logger";
 
 export function cronJob() {
   cron.schedule("0 0 */1 * * *", () => {
-    console.log("Running every hour...");
+    logger.log("Running send email review request cron...");
   });
 }
