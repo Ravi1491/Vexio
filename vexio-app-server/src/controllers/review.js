edeaab5da5c123f52ea24b3956f45103d0620934
Divyansh Rai <divyanshrai27@gmail.com>
2023-12-17 18:09:17 +0530

    reviews api

--- vexio-app-server/src/controllers/review.js
+++ vexio-app-server/src/controllers/review.js
@@ -1,4 +1,5 @@
-import { createReviewReceive, findOneReceived } from "../services/review";
+import { createReviewReceive, findOneReceived, getAllReviewRequests } from "../services/review";
+import { findAllProducts } from "../services/store";
 
 export async function acceptEmailReviews(req, res) {
   try {
@@ -13,7 +14,24 @@ export async function acceptEmailReviews(req, res) {
 
     res.status(200).send({ review });
   } catch (error) {
-    logger.error(error);
+    // logger.error(error);
+    res.status(400).send(error);
+  }
+}
+
+export async function getReviewRequest(req, res) {
+  try {
+    const storeId = req.query.storeId;
+
+    const { rows } = await findAllProducts({ storeId });
+
+    const productIds = await rows.map(a => a.id);
+
+    let reviewsRequested = await getAllReviewRequests({productId: productIds});
+
+    res.status(200).send(reviewsRequested);
+  } catch (error) {
+    console.log(error);
     res.status(400).send(error);
   }
 }
